e4cda1eacea2ae0c99bc8751d9f94401b75d22e4
njjza <njjza@hotmail.com>
2021-02-19 22:01:08 -0800

    Adding point class

---
+++ Point.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file    Point.cpp
+ * @author Henry Jiang 2/19/2021
+ *          
+ * 
+**/
+#include <iostream>
+#include <math>
+
+class Point {
+    public:
+     double x;
+     double y;
+    
+     int cluster;
+     vector get_pos() {
+         vector <double, double> a = 
+     }
+
+     int get_distance(Point x) {
+         
+     }
+};
+
+
