d69c8eb607a1cc34775c01eef0719fcc79c29323
njjza <njjza@hotmail.com>
2021-02-22 20:23:06 -0800

    asd

--- test/Data_Generator_Test.cpp
+++ test/Data_Generator_Test.cpp
@@ -1,9 +1,6 @@
 #include "../include/DataGenerator.hpp"
 
 int main() {
-    generate_data(2, 5, "new");
-    generate_data(10, 5, "asd");
-    generate_data(20, 20, 0.5);
-    generate_data(20, 200, "asd");
+    generate_data(10000, 2*100000, "new");
     return 0;
 }
