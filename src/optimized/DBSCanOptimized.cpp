6814020d2b9cc3bc817b95e3628af074702c1828
njjza <njjza@hotmail.com>
2021-02-22 23:40:58 -0800

    Adding new build

--- src/optimized/DBSCanOptimized.cpp
+++ src/optimized/DBSCanOptimized.cpp
@@ -29,7 +29,7 @@ std::vector<unsigned int> rangeQuery(Points &p, unsigned int pos, double eps){
     return Neighbor;
 }
 
-void DBSCan (Points &p, double eps, unsigned int Minpts){
+void DBSCan (Points p, double eps, unsigned int Minpts){
     int grp_id = 1;
     unsigned int len;
     std::vector<unsigned int> neighbor, neighbor_neighbor;
