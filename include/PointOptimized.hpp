ec916ccfbcbbae7a922246739c24c6f855a05af2
Mingcheng Wu <mingchengwu@system76-pc.localdomain>
2021-03-26 18:40:53 -0700

    add all

--- include/PointOptimized.hpp
+++ include/PointOptimized.hpp
@@ -2,19 +2,21 @@
 #define Point_H
 
 #ifndef POINT_UNDEFIEND
-#define POINT_UNDEFINED 0xffffffff
+#define POINT_UNDEFINED -1
 #endif
 
 #include <iostream>
 #include <vector>
-struct Point {
-     double              x;
-     double              y;
-     unsigned int        grp_id;
+//#include "3d-vectors.hpp"
+
+struct __attribute__ ((aligned(16)))Point {
+     float              x;
+     float              y;
+     float         grp_id;
 };
 //__attribute__((packed));
 
-double get_distance(Point p1, Point p2);
-double get_distance(Point p, double x, double y);
+float get_distance(Point p1, Point p2);
+float get_distance(Point p, double x, double y);
 
 #endif
